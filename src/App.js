3e51ddfe5863601b2f0ecbc439fa40c6b087ff39
vboxuser <vboxuser@blockspan-ubuntu.myguest.virtualbox.org>
2023-06-22 17:48:01 -0600

    images added to dashboard

--- src/App.js
+++ src/App.js
@@ -5,42 +5,44 @@ import './App.css'
 // import CollectionsList from './CollectionsList';
 
 const App = () => {
-    const [collections, setCollections] = useState(null)
+    const [NFTs, setNFTs] = useState(null)
     const [walletAddress, setWalletAddress] = useState('');
-    const [blockchain, setBlockchain] = useState('eth-main');
     const [error, setError] = useState(null);
+    const [blockchain, setBlockchain] = useState('eth-main')
 
-    const fetchCollections = async () => {
-      try {
+    const fetchNFTs = async () => {
+      // const nfts = []
+      // const blockchains = ["eth-main","arbitrum-main","optimism-main" ,"poly-main" ,"bsc-main","eth-goerli"]
+      // for (const index in blockchains) {
+        try {
           const options = {
               method: 'GET',
-              url: `http://localhost:8080/v1/collections/owner/${walletAddress}?chain=${blockchain}`,
+              url: `http://localhost:8080/v1/nfts/owner/${walletAddress}?chain=${blockchain}&include_nft_details=true`,
               headers: {
                   accept: 'application/json',
                   'X-API-KEY': '2jhzbqIWanB8puiqySBIWJVf6Ovp7oPW'
               }
-          };
-          console.log('url', options.url)
+          }
           const response = await axios.request(options);
-          console.log('response', response)
-          setCollections(response.data.results);
+          console.log(response)
+          setNFTs(response.data.results);
           setError(null)
-      } catch (error) {
-          console.error('Error fetching collections:', error);
-          setError('Error: Verify chain and wallet address are valid!');
-      }
-    };
-    
-    const handleBlockchainChange = (event) => {
-        setBlockchain(event.target.value);
-      };
-    
+        } catch (error) {
+            console.error('Error fetching collections:', error);
+            setError('Error: Verify chain and wallet address are valid!');
+        }
+    }
+
     const checkData = (data) => (data ? data : 'N/A');
+
+    const handleBlockchainChange = event => {
+      setBlockchain(event.target.value);
+    };  
 
     return (
         <div className='App'>
           <h1 className='title'>NFT Marketplace Tracker</h1>
-          <p>Select a chain and input wallet address below to see NFT assets.</p>
+          <p>Input wallet address below to see NFT assets.</p>
           <div className='inputContainer'>
             <select name="blockchain" value={blockchain} onChange={handleBlockchainChange}>
               <option value="eth-main">eth-main</option>
@@ -56,32 +58,41 @@ const App = () => {
                 value={walletAddress}
                 onChange={(e) => setWalletAddress(e.target.value)}
               />
-            <button onClick={fetchCollections}>View NFTs</button>
-            {console.log('chain:', blockchain)}
+            <button onClick={fetchNFTs}>View NFTs</button>
           </div>
           {error ? (
             <p className="errorMessage">{error}</p>
-          ) : collections && (
+          ) : NFTs && (
             <table>
+              {console.log(NFTs)}
               <thead>
                 <tr style={{ backgroundColor: '#f2f2f2' }}>
                   <th>NFT Number</th>
-                  <th>Contract Address</th>
-                  <th>Token Type</th>
-                  <th>Total Quantity</th>
-                  <th>Total Tokens</th>
+                  <th>Chain</th>
+                  <th>Image</th>
                 </tr>
               </thead>
               <tbody>
-                {Object.keys(collections).map((index) => (
-                  <tr style={{ backgroundColor: '#f2f2f2' }} key={index}>
-                    <td>{parseInt(index) + 1}</td>
-                    <td>{checkData(collections[index].contract_address)}</td>
-                    <td>{checkData(collections[index].token_type)}</td>
-                    <td>{checkData(collections[index].total_quantity)}</td>
-                    <td>{checkData(collections[index].total_tokens)}</td>
-                  </tr>
-                ))}
+              {NFTs.map((nft, index) => (
+                <tr style={{ backgroundColor: '#f2f2f2' }} key={index}>
+                  <td>{index + 1}</td>
+                  <td>{blockchain}</td>
+                  <td>
+                    <div className="imageContainer">
+                      {nft.nft_details.cached_images && nft.nft_details.cached_images.medium_500_500 ? (
+                        <img 
+                          className="image" 
+                          src={nft.nft_details.cached_images.medium_500_500} 
+                          alt={nft.nft_details.name}/>
+                      ) : (
+                        <div className='message'>
+                          Image not available.
+                        </div>
+                      )}
+                    </div>
+                  </td>
+                </tr>
+              ))}
               </tbody>
             </table>
           )}
