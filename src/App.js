271f3f2500809107e00c8ce5a1f3d816e2fbe009
vboxuser <vboxuser@blockspan-ubuntu.myguest.virtualbox.org>
2023-06-23 15:29:42 -0600

    chain dropdown menu removed

--- src/App.js
+++ src/App.js
@@ -5,66 +5,153 @@ import './App.css'
 // import CollectionsList from './CollectionsList';
 
 const App = () => {
-    const [NFTs, setNFTs] = useState(null)
-    const [walletAddress, setWalletAddress] = useState('');
-    const [error, setError] = useState(null);
-    const [blockchain, setBlockchain] = useState('eth-main')
+    const [walletAddress, setWalletAddress] = useState(null);
+    let [NFTs, setNFTs] = useState([])
+    let [chainArray, setChainArray] = useState([])
+    const [showNoNFTsMessage, setShowNoNFTsMessage] = useState(false);
+    
+    const blockchains = ['eth-main','arbitrum-main','optimism-main','poly-main','bsc-main','eth-goerli']
+    const [ethNFTs, setEthNFTs] = useState(null)
+    const [arbitrumNFTs, setArbitrumNFTs] = useState(null)
+    const [optimismNFTs, setOptimismNFTs] = useState(null)
+    const [polyNFTs, setPolyNFTs] = useState(null)
+    const [bscNFTs, setBscNFTs] = useState(null)
+    const [goerliNFTs, setGoerliNFTs] = useState(null)
+    const [numberOfChains, setNumberOfChains] = useState(0)
+
 
     const fetchNFTs = async () => {
-      // const nfts = []
-      // const blockchains = ["eth-main","arbitrum-main","optimism-main" ,"poly-main" ,"bsc-main","eth-goerli"]
-      // for (const index in blockchains) {
+      
+      setEthNFTs(null)
+      setArbitrumNFTs(null)
+      setOptimismNFTs(null)
+      setPolyNFTs(null)
+      setBscNFTs(null)
+      setGoerliNFTs(null)
+      setNumberOfChains(0)
+
+      for (const index in blockchains) {
+
+        const blockchain = blockchains[index]
         try {
           const options = {
               method: 'GET',
-              url: `http://localhost:8080/v1/nfts/owner/${walletAddress}?chain=${blockchain}&include_nft_details=true`,
+              url: `http://localhost:8080/v1/nfts/owner/${walletAddress}?chain=${blockchain}&include_nft_details=true&page_size=25`,
               headers: {
                   accept: 'application/json',
                   'X-API-KEY': '2jhzbqIWanB8puiqySBIWJVf6Ovp7oPW'
               }
           }
           const response = await axios.request(options);
-          console.log(response)
-          setNFTs(response.data.results);
-          setError(null)
+          setNumberOfChains(prevNumberOfChains => prevNumberOfChains + 1)
+          switch (blockchain) {
+            case 'eth-main':
+              setEthNFTs(response.data.results)
+              break;
+            case 'arbitrum-main':
+              setArbitrumNFTs(response.data.results)
+              break;
+            case 'optimism-main':
+              setOptimismNFTs(response.data.results)
+              break;
+            case 'poly-main':
+              setPolyNFTs(response.data.results)
+              break;
+            case 'bsc-main':
+              setBscNFTs(response.data.results)
+              break;
+            case 'eth-goerli':
+              setGoerliNFTs(response.data.results)
+              break;
+          }
         } catch (error) {
-            console.error('Error fetching collections:', error);
-            setError('Error: Verify chain and wallet address are valid!');
+            // console.error('Error fetching collections:', error);
         }
+      }
+      if (numberOfChains === 0) {
+        setShowNoNFTsMessage(true);
+      } else {
+        setShowNoNFTsMessage(false);
+      }
     }
 
-    const checkData = (data) => (data ? data : 'N/A');
+    // Initialize the NFTs array and the chainsArray so that both are the same length, and
+    // The nft at each index corresponds to the chain at the same index
+    for (let i = 0; i < 6; i++) {
+      const blockchain = blockchains[i]
+      switch (blockchain) {
+        case 'eth-main':
+          if (ethNFTs === null) {
+            continue
+          } else {
+            NFTs = NFTs.concat(ethNFTs)
+            chainArray = chainArray.concat(Array(ethNFTs.length).fill('eth-main'))
+          }
+          break;
+        case 'arbitrum-main':
+          if (arbitrumNFTs === null) {
+            continue
+          } else {
+            NFTs = NFTs.concat(arbitrumNFTs)
+            chainArray = chainArray.concat(Array(arbitrumNFTs.length).fill('arbitrum-main'))
+          }
+          break;
+        case 'optimism-main':
+          if (optimismNFTs === null) {
+            continue
+          } else {
+            NFTs = NFTs.concat(optimismNFTs)
+            chainArray = chainArray.concat(Array(optimismNFTs.length).fill('optimism-main'))
+          }
+          break;
+        case 'poly-main':
+          if (polyNFTs === null) {
+            continue
+          } else {
+            NFTs = NFTs.concat(polyNFTs)
+            chainArray = chainArray.concat(Array(polyNFTs.length).fill('poly-main'))
+          }
+          break;
+        case 'bsc-main':
+          if (bscNFTs === null) {
+            continue
+          } else {
+            NFTs = NFTs.concat(bscNFTs)
+            chainArray = chainArray.concat(Array(bscNFTs.length).fill('bsc-main'))
+          }
+          break;
+        case 'eth-goerli':
+          if (goerliNFTs === null) {
+            continue
+          } else {
+            NFTs = NFTs.concat(goerliNFTs)
+            chainArray = chainArray.concat(Array(goerliNFTs.length).fill('goerli-main'))
+          }
+          break;
+      }
+    }
 
-    const handleBlockchainChange = event => {
-      setBlockchain(event.target.value);
-    };  
+
+
+    const checkData = (data) => (data ? data : 'N/A');
 
     return (
         <div className='App'>
           <h1 className='title'>NFT Marketplace Tracker</h1>
           <p>Input wallet address below to see NFT assets.</p>
           <div className='inputContainer'>
-            <select name="blockchain" value={blockchain} onChange={handleBlockchainChange}>
-              <option value="eth-main">eth-main</option>
-              <option value="arbitrum-main">arbitrum-main</option>
-              <option value="optimism-main">optimism-main</option>
-              <option value="poly-main">poly-main</option>
-              <option value="bsc-main">bsc-main</option>
-              <option value="eth-goerli">eth-goerli</option>
-            </select>
             <input
-                type="text"
-                placeholder="Wallet Address"
+                type='text'
+                placeholder='Wallet Address'
                 value={walletAddress}
                 onChange={(e) => setWalletAddress(e.target.value)}
               />
             <button onClick={fetchNFTs}>View NFTs</button>
           </div>
-          {error ? (
-            <p className="errorMessage">{error}</p>
-          ) : NFTs && (
+          {showNoNFTsMessage && walletAddress !== null? (
+            <p className='errorMessage'>No NFTs owned by this wallet!</p>
+          ) : NFTs.length !== 0 && (
             <table>
-              {console.log(NFTs)}
               <thead>
                 <tr style={{ backgroundColor: '#f2f2f2' }}>
                   <th>NFT Number</th>
@@ -76,12 +163,12 @@ const App = () => {
               {NFTs.map((nft, index) => (
                 <tr style={{ backgroundColor: '#f2f2f2' }} key={index}>
                   <td>{index + 1}</td>
-                  <td>{blockchain}</td>
+                  <td>{chainArray[index]}</td>
                   <td>
-                    <div className="imageContainer">
+                    <div className='imageContainer'>
                       {nft.nft_details.cached_images && nft.nft_details.cached_images.medium_500_500 ? (
                         <img 
-                          className="image" 
+                          className='image' 
                           src={nft.nft_details.cached_images.medium_500_500} 
                           alt={nft.nft_details.name}/>
                       ) : (
